22fa150e51da40bb5b3ef80c2ba7a41237ba8d96
fulvi <fulvi@192.168.0.3>
2019-03-01 17:58:24 -0300

    documentation

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>PHPDesingPatterns</title>
     </head>
     <body>
+        <h1>PHPDesingPatterns</h1>
+        <br/>
         <?php
-        // put your code here
+        phpinfo();
         ?>
     </body>
 </html>
