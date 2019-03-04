3b001d21e5dbf8677cba91ffb72584e482dce50a
fulvi <fulvi@192.168.0.3>
2019-03-04 05:04:32 -0300

    documentation

---
+++ creational/factory/FactoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fulvi
+ */
+interface FactoryInterface {
+    public static function create($clsName);
+}
